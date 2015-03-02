035dd68115065e28b6c5e21e1821691e9fae80bd
Hardik Joshi <h4hardikonly@gmail.com>
2015-03-02 10:40:54 +0530

    modified file permissions and changed db settings

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn hellodjango.wsgi --log-file -
+web: gunicorn django_poll_app.wsgi --log-file -
--- django_poll_app/settings.py
+++ django_poll_app/settings.py
@@ -59,6 +59,14 @@ WSGI_APPLICATION = 'django_poll_app.wsgi.application'
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+DATABASES = {}
 DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
@@ -68,8 +76,8 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
@@ -89,9 +97,3 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-STATIC_URL = '/static/'
